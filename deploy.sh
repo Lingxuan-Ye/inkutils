96825c3657df5ca86dff71f0467ff81749e1388c
Lingxuan Ye <lingxuan.ye@icloud.com>
2023-01-04 07:59:01 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
+## create symbolic link for dotfiles
+ln -s ~/inkutils/dotfiles/.bashrc ~/.bashrc
+ln -s ~/inkutils/dotfiles/.gitconfig ~/.gitconfig
+
 ## create venv
-python -m venv ./venv/
+rm -r -f ~/venv/ink/ && python -m venv ~/venv/ink/
 
 ## activate venv
-source ./venv/Scripts/activate
+source ~/venv/ink/Scripts/activate
 
 ## upgrade pip
 pip install --upgrade pip
@@ -12,6 +16,6 @@ pip install --upgrade pip
 ## install site-packages
 pip install -r ./requirements.txt
 
-
+# exit prompt
 echo
 read -p 'press any key to exit...' -n 1
